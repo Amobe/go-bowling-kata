7a933da469fcac8fd6e6970a8d010300774f865a
lex <lex.liu.dlink@gmail.com>
2019-02-22 10:04:17 +0800

    Feature: Implement the Roll and Score function

--- src/game/game.go
+++ src/game/game.go
@@ -1,12 +1,16 @@
 package game
 
+// Game represents one round of the bowling game.
 type Game struct {
 	score int
 }
 
+// Roll counts score with the hitting pins of each roll.
 func (g *Game) Roll(pins int) {
+	g.score += pins
 }
 
+// Score returns the score of the bowling game.
 func (g *Game) Score() int {
-	return -1
+	return g.score
 }
