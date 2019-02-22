403369adf8536bfa688098b1fd6410cf8bf6e13a
lex <lex.liu.dlink@gmail.com>
2019-02-22 16:50:55 +0800

    Feature: Implement function to calculate spare bonus
    Store the hit pins for each roll for calculating the spare bonus score.

--- src/game/game.go
+++ src/game/game.go
@@ -2,15 +2,29 @@ package game
 
 // Game represents one round of the bowling game.
 type Game struct {
-	score int
+	score    int
+	rolls    [21]int
+	frameCnt int
 }
 
 // Roll counts score with the hitting pins of each roll.
 func (g *Game) Roll(pins int) {
-	g.score += pins
+	g.rolls[g.frameCnt] = pins
+	g.frameCnt++
 }
 
 // Score returns the score of the bowling game.
 func (g *Game) Score() int {
+	frameIndex := 0
+	for frameIndex < g.frameCnt {
+		if g.rolls[frameIndex]+g.rolls[frameIndex+1] == 10 {
+			g.score += 10 + g.rolls[frameIndex+2]
+			frameIndex += 2
+		} else {
+			g.score += g.rolls[frameIndex]
+			frameIndex++
+		}
+
+	}
 	return g.score
 }
